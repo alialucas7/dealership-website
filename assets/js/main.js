7c40cd36c50045d6c2ee52bafd653d443f73b107
Lucas <lukitis2@gmail.com>
2022-03-31 14:00:18 -0300

    funcionalidad a la muestra de productos con javascript y swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,8 +38,29 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== POPULAR SWIPER ===============*/
+let swiperPopular = new Swiper(".popular__container", {
+    loop: true,
+    spaceBetween: 24,
+    slidesPreview: 'auto',
+    grabCursor: true,
 
-
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    breakpoints: {
+       
+        768: {
+          slidesPerView: 3,
+        
+        },
+        1024: {
+        
+          spaceBetween: 42,
+        },
+      },
+  });
+ 
 /*=============== MIXITUP FILTER FEATURED ===============*/
 
 

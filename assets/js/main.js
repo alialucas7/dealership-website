8c656abd554d4fa2875a5ae877cb7a373bacc5bd
Lucas <lukitis2@gmail.com>
2022-04-02 21:03:12 -0300

    animacion a la pagina con ScrollReveal(javascript)

--- assets/js/main.js
+++ assets/js/main.js
@@ -106,7 +106,30 @@ window.addEventListener('scroll', scrollUp)
        }
        window.addEventListener('scroll', scrollActive)
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+/*=============== SCROLL REVEAL ANIMATION -- animaciones de la pagina ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+
+})
+
+sr.reveal(`.home__title, .popular__container, .features__img, .featured__filters`)
+sr.reveal(`.home__subtitle`, {delay: 500})
+sr.reveal(`.home__elec`, {delay: 600})
+sr.reveal(`.home__img`, {delay: 800})
+sr.reveal(`.home__car-data`, {delay: 900, interval: 100, origin: 'bottom'})
+sr.reveal(`.home__button`, {delay: 1000,  origin: 'bottom'})
+sr.reveal(`.about__group, .offer__data`, { origin: 'left'})
+sr.reveal(`.about__data, .offer__img`, { origin: 'right'})
+sr.reveal(`.features__map`, {delay:600, origin: 'bottom'})
+sr.reveal(`.features__card`, { interval: 300})
+sr.reveal(`.featured__card, .logos__content`, { interval: 100})
+
+
+
+
 
 
 
